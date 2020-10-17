7cd80a496756ca343c4105492dd4ab503bcd0d97
vojdan <vojdank@gmail.com>
2020-10-18 10:10:57 +1100

    c leanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,12 @@
 import * as functions from 'firebase-functions';
 
-// export const empty = functions.https.onRequest((request, response) => {
-// 	functions.logger.info("Hello logs!", {structuredData: true});
-// 	response.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-// 	response.send("Hello from Firebase!");
-// });
-//
-// export const importOnly = functions.https.onRequest(async (request, response) => {
-// 	functions.logger.info("abn start");
-// 	// @ts-ignore
-// 	const result = await (await import('./imports/AbnLookup')).default(request);
-// 	// return FnResponses.sendHttpResponseJson(response, result) as any;
-//
-// 	functions.logger.info("abn end");
-// 	response.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-// 	response.send(result);
-// });
+export const empty = functions.https.onRequest((request, response) => {
+	functions.logger.info("Hello logs!", {structuredData: true});
+	response.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+	response.send("Hello from Firebase!");
+});
 
-export const importOnlyOld = functions.https.onRequest(async (request, response) => {
+export const importOnly = functions.https.onRequest(async (request, response) => {
 	functions.logger.info("abn start");
 	// @ts-ignore
 	const result = await (await import('./imports/AbnLookup')).default(request);
